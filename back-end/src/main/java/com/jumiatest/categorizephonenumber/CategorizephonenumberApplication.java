4692092bfc8a9ea4b410c16a814617dda8722fba
KareemElsayed <kareemelsayedelbadawy@gmail.com>
2022-12-04 10:36:58 +0200

    update github

--- back-end/src/main/java/com/jumiatest/categorizephonenumber/CategorizephonenumberApplication.java
+++ back-end/src/main/java/com/jumiatest/categorizephonenumber/CategorizephonenumberApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cache.annotation.EnableCaching;
 @EnableCaching
 public class CategorizephonenumberApplication {
 
+    // update git.
     public static void main(String[] args) {
         SpringApplication.run(CategorizephonenumberApplication.class, args);
     }
